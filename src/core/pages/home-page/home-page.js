56950c95fc08e59f2c380eba3503b4cad294b817
Gabriel Cano <gabrielcano@hotmail.fr>
2021-06-09 23:55:18 +0200

    Login and logout logic

--- src/core/pages/home-page/home-page.js
+++ src/core/pages/home-page/home-page.js
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../../domains/user";
 
-const HomePage = () => <div>welcome !</div>;
+const HomePage = () => {
+  const { discordAuthInfo, discordLogout } = useContext(AuthContext);
+  return (
+    <div>
+      <pre>{JSON.stringify(discordAuthInfo, null, 2)}</pre>
+      <button type="button" onClick={discordLogout}>Logout</button>
+    </div>
+  );
+};
 
 export default HomePage;
--- src/domains/user/components/auth-context/auth-context.js
+++ src/domains/user/components/auth-context/auth-context.js
@@ -1,17 +1,71 @@
-import React, { createContext } from "react";
+import React, { useEffect, useState, createContext } from "react";
 import PropTypes from "prop-types";
 import qs from "qs";
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
+import {
+  validateAccessCodeCsrf, discordAuthenticate, discordStartLogin, cleanDiscordAuthCsrf, revokeToken,
+} from "../../utils/auth-utils";
+import {
+  getLocalstorageDiscordAuthInfo, setLocalstorageDiscordAuthInfo, clearLocalstorageDiscordAuthInfo,
+} from "../../utils/auth-localstorage-utils";
 
 const AuthContext = createContext({});
 
 const AuthProvider = ({ children }) => {
+  const [discordAuthInfo, setDiscordAuthInfo] = useState({});
+
   const location = useLocation();
+  const browserHistory = useHistory();
+
   const queryParam = qs.parse(location.search, { ignoreQueryPrefix: true });
-  console.log(location);
+
+  const discordLogin = () => {
+    if (!discordAuthInfo.accessToken) {
+      discordStartLogin();
+    }
+  };
+
+  const discordLogout = async () => {
+    const { refreshToken, accessToken } = discordAuthInfo;
+    if (refreshToken) {
+      await revokeToken(refreshToken);
+    }
+    if (accessToken) {
+      await revokeToken(accessToken);
+    }
+    // Clear stored discord auth info
+    clearLocalstorageDiscordAuthInfo();
+
+    // Clear context auth info
+    setDiscordAuthInfo({});
+  };
+
+  const setupAuthentication = () => {
+    const { code, state } = queryParam;
+    if (!discordAuthInfo?.accessToken && code && state && validateAccessCodeCsrf(state)) {
+      cleanDiscordAuthCsrf();
+
+      discordAuthenticate(code).then((response) => {
+        const { access_token: accessToken, refresh_token: refreshToken, scope } = response.data;
+        setDiscordAuthInfo({ accessToken, refreshToken, scope });
+        setLocalstorageDiscordAuthInfo({ accessToken, refreshToken, scope });
+        browserHistory.replace(location.pathname);
+      });
+    }
+  };
+
+  const initAuthentication = () => {
+    const storedDiscordAuthInfo = getLocalstorageDiscordAuthInfo();
+    if (storedDiscordAuthInfo && !discordAuthInfo.accessToken) {
+      setDiscordAuthInfo(storedDiscordAuthInfo);
+    }
+  };
+
+  useEffect(initAuthentication, []);
+  useEffect(setupAuthentication, [queryParam]);
 
   return (
-    <AuthContext.Provider value={{}}>
+    <AuthContext.Provider value={{ discordAuthInfo, discordLogin, discordLogout }}>
       {children}
     </AuthContext.Provider>
   );
